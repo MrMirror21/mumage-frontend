2dac66bb01743107c132ff8ea66735081fe29e35
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2023-09-20 14:23:55 +0900

    initial commit
    - 프로젝트 기본 환경 세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Landing = () => {
+  return (
+    <>
+      <StyledTitle>Landing</StyledTitle>
+    </>
+  )
+}
+
+export default Landing
+
+const StyledTitle = styled.div``;

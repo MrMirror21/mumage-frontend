2e0b3605fbbfd9b2493edb3b2b1746dfc830e926
이소연 <sy040103@naver.com>
2023-09-22 01:41:24 +0900

    feat: Recom 컴포넌트 라우팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
-
+import Recom from './pages/Recom';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
+				<Route path="/Recom" element={<Recom />} />
       </Routes>
     </>
   );
---
+++ src/pages/Recom.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recom = () => {
+  return (
+    <>Recom</>
+  )
+}
+
+export default Recom

9d6cfb8b31f8319b912594b8ca846e1f0d97f774
이소연 <sy040103@naver.com>
2023-09-29 01:26:09 +0900

    feat: 임시 데이터로 피드 띄우기
    SelectBox에 따라 임시 데이터 가져와 Section에 적용

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,16 @@
 import React, {useState} from 'react'
 import SelectBox from './SelectBox'
+import {FakeDataArr} from '../store/FakeDataArr'
 
 const Section = () => {
   const [sectionValue, setSectionValue] = useState('종합');
 
+  const matchedData = FakeDataArr.filter(data => data["장르"] === sectionValue);
+
   return (
     <div>
       <SelectBox onChange = {(value) => setSectionValue(value)} />
-      <>{sectionValue}</>
+      {matchedData.map(data => (data["내용"]))}
     </div>
   )
 }

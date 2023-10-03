313709984d4823ba0ff7ee6e05563c722f4292b8
이소연 <sy040103@naver.com>
2023-10-03 18:11:06 +0900

    페이지네이션 - 이전, 다음 버튼
    게시물이 한 페이지 당 9개씩 보이도록 함

--- src/components/Section.js
+++ src/components/Section.js
@@ -19,21 +19,50 @@ const GridItem = styled.div`
   aspect-ratio: 1 / 1;
 `;
 
+const Button = styled.button`
+  padding-top : 7px;
+  width: 60px;
+  padding: 8px;
+  border-radius: 12px;
+  background-color: #ffffff;
+  align-self: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+`;
+
 const Section = () => {
   const [sectionValue, setSectionValue] = useState('종합');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
 
   const matchedData = FakeDataArr.filter(data => data["장르"] === sectionValue);
+  const totalPage = Math.ceil(matchedData.length / itemsPerPage); 
+  const displayedData = matchedData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const handleUpPage = () => {
+    setCurrentPage(currentPage + 1);
+  }
+  const handleDownPage = () => {
+    setCurrentPage(currentPage - 1);
+  }
 
   return (
     <div>
       <SelectBoxContainer>
-        <SelectBox onChange = {(value) => setSectionValue(value)} />
+        <SelectBox onChange = {(value) => {
+          setSectionValue(value);
+          setCurrentPage(1);
+        }} />
       </SelectBoxContainer>
       <GridContainer>
-        {matchedData.map((data, index) => (
+        {displayedData.map((data, index) => (
           <GridItem key={index}>{data["내용"]}</GridItem>
         ))}
       </GridContainer>
+      <div>
+      <Button onClick={handleDownPage} disabled={currentPage === 1}>이전</Button>
+        <Button onClick={handleUpPage} disabled={currentPage === totalPage}>다음</Button>
+      </div>
     </div>
   )
 }

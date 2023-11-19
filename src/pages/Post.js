756550a03beb1fb04d9a916dd27246cf2fd9c708
이소연 <sy040103@naver.com>
2023-11-19 21:47:10 +0900

    style: Post 이전 버튼 디자인 적용

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {useParams, useLocation} from 'react-router-dom';
+import '../styles/Post.css'
 import Icon from '../components/Icon';
 import {GridItem, Button} from '../components/Section';
 import styled from 'styled-components';
 import {useNavigate} from 'react-router-dom';
 import {FakeDataArr} from '../store/FakeDataArr';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
 
 const Post = () => {
   const {postId} = useParams();
@@ -25,7 +29,10 @@ const Post = () => {
 
     <>
       <Icon/>
-      <Button onClick={goBackWithState}>&lt;&lt;</Button>
+      <div className="arrow-left-menu">
+        <button id="arrow-left-icon" onClick={goBackWithState}><FontAwesomeIcon icon={faArrowLeft} style = {{backgroundColor:'white', fontSize:'20px'}} /></button>
+        <button id="like"><FontAwesomeIcon icon={faHeart} style = {{backgroundColor:'white', fontSize:'20px'}} /></button>
+      </div>
       <SelectGridContainer>
         <GridItem><img src={postData["imageUrl"]}></img></GridItem>
       </SelectGridContainer>

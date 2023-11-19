70fed30bce78bf27c53c6a517406f05cef4d1c1f
이소연 <sy040103@naver.com>
2023-11-19 23:14:57 +0900

    styel: Post 디자인 적용

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -2,13 +2,23 @@ import React from 'react';
 import {useParams, useLocation} from 'react-router-dom';
 import '../styles/Post.css'
 import Icon from '../components/Icon';
-import {GridItem, Button} from '../components/Section';
 import styled from 'styled-components';
 import {useNavigate} from 'react-router-dom';
 import {FakeDataArr} from '../store/FakeDataArr';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
 import {faHeart} from '@fortawesome/free-solid-svg-icons';
+
+export const GridItem = styled.div`
+  aspect-ratio: 1 / 1;
+  position : relative;
+  img {
+    position : absolute;
+    width : 100%;
+    height : 100%,
+    object-fit : cover;
+  }
+`;
 
 const Post = () => {
   const {postId} = useParams();
@@ -33,9 +43,20 @@ const Post = () => {
         <button id="arrow-left-icon" onClick={goBackWithState}><FontAwesomeIcon icon={faArrowLeft} style = {{backgroundColor:'white', fontSize:'20px'}} /></button>
         <button id="like"><FontAwesomeIcon icon={faHeart} style = {{backgroundColor:'white', fontSize:'20px'}} /></button>
       </div>
+      <div id="nickname">{postData["nickname"]}</div>
       <SelectGridContainer>
         <GridItem><img src={postData["imageUrl"]}></img></GridItem>
       </SelectGridContainer>
+      <div className='like-num-menu'>
+        <div id="likeNum" >
+          <div id="like-num">{postData["좋아요"]}</div>
+          <FontAwesomeIcon id="like-num-icon"icon={faHeart} style = {{backgroundColor:'white', fontSize:'20px'}} />
+        </div>
+      </div>
+      <div id="text">
+        <div id="title">{postData["title"]}</div>
+        <div id="context">{postData["context"]}</div>
+      </div>
     </>
   );
 }

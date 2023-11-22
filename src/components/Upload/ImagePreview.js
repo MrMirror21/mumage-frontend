355f09f12b5cf3f5e972f0bcfee22518ef8ac9be
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2023-11-22 15:52:38 +0900

    style : 이미지 생성 후 UI 수정

--- src/components/Upload/ImagePreview.js
+++ src/components/Upload/ImagePreview.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const ImagePreview = ({imageURL}) => {
+const ImagePreview = ({imageURL, postData, setImage}) => {
   return (
   <>
     <ImageSection>
       <ImageContainer>
-        {imageURL.map((image) => <img src={image} alt="이미지 미리보기" />)}
+        {imageURL.map((image) => 
+        <GridItem className={postData.imageUrl === image ? " selected" : ""}
+        >
+          <img 
+            src={image} 
+            className={postData.imageUrl === image ? "image selected" : "image"}
+            alt="이미지 미리보기" 
+            onClick={()=>setImage({...postData, "imageUrl" : image})}/>          
+        </GridItem>        
+        )}
       </ImageContainer>
     </ImageSection>
   </>
@@ -21,12 +30,28 @@ const ImageSection = styled.div`
   align-items: center;
   justify-content: center;
   width: 100vw;
+  height: 700px;
+
 `;
 
 const ImageContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  width: 700px;
+  height: 600px;
+  grid-template-columns : 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
   justify-content: center;
-  align-items: center;
-  background: #f1f1f1;
+  place-items: center;
+  .image{
+    width: 300px;
+    margin: 10px;
+    object-fit: fill;
+  }
+  .selected {
+    background: #888BF4;
+  }
+`;
+
+const GridItem = styled.div`
+  border-radius: 10px;
 `;
